3f71555c5b0af5f2f398886c56b0f8b767955d30
TalitaCumiAraujo <talitacumi.araujo@gmail.com>
2022-03-20 21:03:39 -0300

    teste

--- src/routes/categoria.routes.ts
+++ src/routes/categoria.routes.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import CategoriaController from "../controllers/CategoriaController";
-import authValidator from "../auth/authValidator";
 const categoriaRoutes = Router();
 
-categoriaRoutes.post("/categorias",authValidator, CategoriaController.create);
-categoriaRoutes.get("/categorias", authValidator, CategoriaController.listing);
-categoriaRoutes.get("/categorias/:id", authValidator, CategoriaController.getById);
-categoriaRoutes.delete("/categorias/:id", authValidator, CategoriaController.delete);
-categoriaRoutes.put("/categorias", authValidator, CategoriaController.update);
+categoriaRoutes.post("/categorias", CategoriaController.create);
+categoriaRoutes.get("/categorias", CategoriaController.listing);
+categoriaRoutes.get("/categorias/:id", CategoriaController.getById);
+categoriaRoutes.delete("/categorias/:id", CategoriaController.delete);
+categoriaRoutes.put("/categorias", CategoriaController.update);
 
 module.exports = categoriaRoutes;
